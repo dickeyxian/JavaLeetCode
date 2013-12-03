e65af1a7eb016fd0e0939b21b66c0c6a2f81010a
xianda <xiandada@12.com>
2013-12-03 22:00:21 +0800

    update

---
+++ src/leetcode/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class RemoveNthNodeFromEndofList {
+	 public ListNode removeNthFromEnd(ListNode head, int n) {
+		 
+	 }
+	public static void main(String[] args) {
+
+	}
+
+}
