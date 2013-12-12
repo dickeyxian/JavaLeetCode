782fd2bf26e04e8d560620344ff364cd85858678
xianda <xiandada@126.com>
2013-12-12 20:21:26 +0800

    count and say

--- src/leetcode/CountandSay.java
+++ src/leetcode/CountandSay.java
@@ -5,21 +5,30 @@ public class CountandSay {
 		if (n == 0) {
 			return "";
 		}
-		StringBuffer sb = new StringBuffer("1");
 		if (n == 1) {
 			return "1";
 		}
+		StringBuffer sb = new StringBuffer("1");
+	
 		for (int i = 2; i <= n; i++) {
+			StringBuffer result = new StringBuffer();
 			for (int j = 0; j < sb.length(); j++) {
-				
+				int count = 1;
+				while (j < sb.length()-1 && sb.charAt(j + 1) == sb.charAt(j)) {
+					count++;
+					j++;
+				}
+				result.append(count);
+				result.append(sb.charAt(j));
 			}
+			sb = result;
 		}
-		return "";
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 2; ++i) {
-			System.out.println(i);
+		for (int i = 1; i < 5; ++i) {
+			System.out.println(countAndSay(6));
 		}
 	}
 
